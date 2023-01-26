b49efd9bfd0899c4e081f57841999266fe35251b
Vlad <m.vlad.popa@gmail.com>
2023-01-25 22:55:44 -0500

    first draft of checker code. hope it works

--- code.rb
+++ code.rb
@@ -1,3 +1,5 @@
+## Class to hold the 4-color code that is created
+
 class Code
   def initialize(colors)
     @color_code = colors.sample(4)
@@ -7,4 +9,14 @@ class Code
     @color_code.each_with_index {|c,i| puts "#{c.bg} #{c.name}, #{i} #{c.reset}" }
   end
 
+  def check(player_code)
+    colors = @color_code.map { |c| c.name }
+    checker = colors.zip(player_code)
+    cor_pos_col = checker.count { |i| i.reduce(:eql?) }
+    cor_col = checker.uniq.count { |i| colors.include?(i) }
+    result = Array.new(4, '')
+    cor_pos_col.times { result.unshift('pos_col').pop }
+    cor_col.times { result.unshift('col').pop }
+    result
+  end
 end
